43b90b5a996c8957ca0c388ae21c6aace6f54258
gustavo <gus.inacio@hotmail.com>
2023-05-02 09:41:09 -0300

    feat: mui css cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
+import AuthProvider from '@/context/auth'
+import { createEmotionCache } from '@/styles/emotionCache'
 import '@/styles/globals.css'
+import {
+  CacheProvider,
+  EmotionCache,
+  ThemeProvider,
+  useTheme,
+} from '@emotion/react'
+import { CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clienteSideEmotionCache = createEmotionCache()
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clienteSideEmotionCache,
+}: MyAppProps) {
+  const theme = useTheme()
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
