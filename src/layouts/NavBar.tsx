ecfd178a3dd84077864cc2cdd7505bc2f130ee4b
gustavo <gus.inacio@hotmail.com>
2023-05-16 19:52:36 -0300

    feat: add logout and login to menu

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -18,12 +18,21 @@ import {
   Menu,
   MenuItem,
   Toolbar,
+  Typography,
 } from '@mui/material'
+import { Coda } from 'next/font/google'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
+const coda = Coda({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 export default function Navbar() {
-  const { user } = useAuth()
+  const { user, logout } = useAuth()
+  const router = useRouter()
 
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
   const [openDrawer, setOpenDrawer] = useState(false)
@@ -60,33 +69,72 @@ export default function Navbar() {
               >
                 <MenuOutlined />
               </IconButton>
+            </Box>
+            <Logo sx={{ display: { xs: 'flex', md: 'none' } }} />
+            <Box display="flex">
               <Box
                 sx={{
                   display: { md: 'flex', xs: 'none' },
                   alignItems: 'center',
                   justifyContent: 'space-evenly',
-                  width: '200px',
+                  width: '300px',
                 }}
               >
-                <Link href="/fac">FAC</Link>
-                <Link href="/manual">Manual</Link>
-                <Link href="/painel">Painel</Link>
+                <Link
+                  href="/fac"
+                  style={{ textDecoration: 'none', color: 'white' }}
+                >
+                  <Typography
+                    sx={{ fontSize: '15px', letterSpacing: '0.3em' }}
+                    className={coda.className}
+                  >
+                    FAC
+                  </Typography>
+                </Link>
+                <Link
+                  href="/manual"
+                  style={{ textDecoration: 'none', color: 'white' }}
+                >
+                  <Typography
+                    sx={{ fontSize: '15px', letterSpacing: '0.3em' }}
+                    className={coda.className}
+                  >
+                    MANUAL
+                  </Typography>
+                </Link>
+                <Link
+                  href="/painel"
+                  style={{ textDecoration: 'none', color: 'white' }}
+                >
+                  <Typography
+                    sx={{ fontSize: '15px', letterSpacing: '0.3em' }}
+                    className={coda.className}
+                  >
+                    PAINEL
+                  </Typography>
+                </Link>
               </Box>
+              <IconButton onClick={handleOpenUserMenu}>
+                <Avatar
+                  variant="circular"
+                  src={user?.avatar_url}
+                  alt={user?.name ?? 'L'}
+                />
+              </IconButton>
             </Box>
-            <Logo sx={{ display: { xs: 'flex', md: 'none' } }} />
-            <IconButton onClick={handleOpenUserMenu}>
-              <Avatar
-                variant="circular"
-                src={user?.avatar_url}
-                alt={user?.name}
-              />
-            </IconButton>
             <Menu
               open={!!anchorElUser}
               anchorEl={anchorElUser}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
               onClose={handleCloseUserMenu}
             >
-              <MenuItem>Sair</MenuItem>
+              {user ? (
+                <MenuItem onClick={logout}>Sair</MenuItem>
+              ) : (
+                <MenuItem onClick={() => router.push('/signIn')}>
+                  Entrar
+                </MenuItem>
+              )}
             </Menu>
           </Box>
         </Toolbar>
