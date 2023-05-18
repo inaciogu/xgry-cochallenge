f18abe98775a4a114531758359768a280e8e5615
gustavo <gus.inacio@hotmail.com>
2023-05-18 10:48:36 -0300

    feat: external problems page

--- src/pages/external-problems/index.tsx
+++ src/pages/external-problems/index.tsx
@@ -4,24 +4,24 @@ import Dashboard from '@/layouts/Dashboard'
 import Head from 'next/head'
 import { useState } from 'react'
 
-export default function CarDashboard() {
-  const [carImage, setCarImage] = useState('/painel.png')
+export default function ExternalProblems() {
+  const [carImage, setCarImage] = useState('/carro.png')
 
   const toggleCarImage = () => {
-    if (carImage === '/painel.png') {
-      setCarImage('/painel-detalhes.png')
+    if (carImage === '/carro.png') {
+      setCarImage('/carro-detalhes.png')
       return
     }
-    setCarImage('/painel.png')
+    setCarImage('/carro.png')
   }
 
   return (
     <>
       <Head>
-        <title>Pilotas.Co | Painel</title>
+        <title>Pilotas.Co | Problemas externos</title>
         <meta
           name="description"
-          content="pilotas co website car dashboard page"
+          content="pilotas co website external problems page"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/wheel.ico" />
